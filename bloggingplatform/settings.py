8913f8ac1e0984ee71b88c5c6fe85b470bd921ee
Alice Campkin <alice.campkin@zoodigital.com>
2020-12-24 17:41:33 +0000

    adds django_countries for user profile

--- bloggingplatform/settings.py
+++ bloggingplatform/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_countries',
     'ckeditor',
     'crispy_forms',
     'user',
