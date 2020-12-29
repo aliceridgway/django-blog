43040b79f0b9ea3530cc1ba4493310fccc669299
Alice Campkin <alice.campkin@zoodigital.com>
2020-12-29 12:41:27 +0000

    Registers Follower model

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,8 @@
-from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from .models import User, Profile
+from .models import User, Profile, Follower
 from .forms import CustomUserCreationForm, UserChangeForm
 
 
@@ -13,19 +11,19 @@ class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = CustomUserCreationForm
 
-    list_display = ('email', 'first_name','last_name','is_admin')
+    list_display = ('email', 'first_name', 'last_name', 'is_admin')
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name','last_name', 'username')}),
-        ('Permissions', {'fields': ('is_admin','is_staff', 'is_active')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'username')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'is_active')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name','last_name', 'password1', 'password2'),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
         }),
     )
     search_fields = ('email',)
@@ -33,9 +31,9 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = ()
 
 
-
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
+admin.site.register(Follower)
 
 # Groups are not required as we're not using the permissions mixin with our User model
 admin.site.unregister(Group)
